173c887b8ac28eaaba46e02d23a67f170023d8e8
Brandon <leebran@onid.oregonstate.edu>
2015-06-29 20:14:07 -0700

    Finished part 1 of closures

--- Closures.playground/Contents.swift
+++ Closures.playground/Contents.swift
@@ -49,3 +49,34 @@ let tripleFunction = { (i: Int) -> Int in return i * 3 }
 
 // Rule #3 - (Implicit returns from single expression closures) If we have a single expression closure, then the return value is implicit
 [1,2,3,4,5].map({ i in i * 3 })
+
+// Or...
+
+// Rule #4 - (Shorthand Arguement Names) Swift provides shorthand argument names for inline closures. Ex - $0, $1, $2
+[1,2,3,4,5].map({ $0 * 3 })
+
+// Or...
+
+// Rule #5 - (Trailing Closure) If the closure expression is the last arg to a function, you can move the expression outside the parenthesis of the function call and write it as a trailing closure instead.
+
+// Example 1
+[1,2,3,4,5].map() { $0 * 3 }
+
+// Example 2
+[1,2,3,4,5].map() {
+    (var digit) -> Int in
+    if digit % 2 == 0 {
+        return digit/2
+    }
+    else {
+        return digit
+    }
+}
+
+// Or...
+
+// Rule #6 - (Ignoring Parenthesis) If a closure expression is the only argument to a function, then you can get rid of the function's parenthesis
+
+[1,2,3,4,5].map() { $0 * 3 }
+
+// Each of these rules is a chapter of Apple's documentation book
