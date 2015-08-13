e6b44fe2fb548ec5bb8de48fe5df3623bf52ff93
Brandon <leebran@onid.oregonstate.edu>
2015-08-13 16:25:06 -0700

    Added another example of loose relationships with prototypes

--- Protocols.playground/Contents.swift
+++ Protocols.playground/Contents.swift
@@ -91,6 +91,7 @@ class SalariedEmployee: Employee {
 
 // Modeling Loose Relationships
 
+// Example 1 - Smoothie
 protocol Blendable {
     func blend()
 }
@@ -144,3 +145,35 @@ let ingredients: [Blendable] = [strawberry, chocolateMilk]
 // Versus - let ingredients: [Blendable] = [strawberry, chocolateMilk, cheddar]
 
 makeSmoothie(ingredients)
+
+// Example 2 - Random Number Generator
+protocol RandomNumberGenerator {
+    func random() -> Double
+}
+
+class LinearCongruentialGenerator: RandomNumberGenerator {
+    var lastRandom = 42.0
+    let m = 139968.0
+    let a = 3877.0
+    let c = 29573.0
+    
+    func random() -> Double {
+        lastRandom = ((lastRandom * a + c) % m)
+        return lastRandom/m
+    }
+}
+
+class Dice {
+    let sides: Int
+    let generator: RandomNumberGenerator
+    init(sides: Int, generator:RandomNumberGenerator) {
+        self.sides = sides
+        self.generator = generator
+    }
+    
+    func roll() -> Int {
+        return Int(generator.random() * Double(sides)) + 1
+    }
+}
+
+var d6 = Dice(sides: 6, generator: LinearCongruentialGenerator())
