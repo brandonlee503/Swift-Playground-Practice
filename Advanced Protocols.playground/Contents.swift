2b299b04b45bd40f08e79a09fef45da9c53f84ce
Brandon <leebran@onid.oregonstate.edu>
2015-08-14 13:06:10 -0700

    Added car example

--- Advanced Protocols.playground/Contents.swift
+++ Advanced Protocols.playground/Contents.swift
@@ -1,5 +1,39 @@
-//: Playground - noun: a place where people can play
+// Advanced Protocols
 
-import UIKit
+protocol Printable {
+    func description() -> String
+}
 
-var str = "Hello, playground"
+// "[someKey: someValue, anotherKey: anotherValue]"
+
+protocol PrettyPrintable: Printable {
+    func prettyDescription() -> String
+}
+
+// Vehicle Protocol
+
+protocol Brakeable {
+    var brakes: String { get }
+    func stop()
+    func slamOnBrakes()
+}
+
+protocol Drivable {
+    var steeringWheel: String { get }
+    var tires: String { get set }
+    func startEngine()
+    func stopEngine()
+    func turnLeft()
+    func turnRight()
+    func reverse()
+}
+
+// Eg. - Motorcycle
+protocol Vehicle: Brakeable, Drivable {
+    var numberOfSeats: Int { get }
+}
+
+// Eg. - Cargit
+protocol Car: Brakeable, Drivable {
+    var numberOfDoors: Int { get }
+}
