802920a76d1850641d25d1d65bccb7e07468cf24
Brandon <leebran@onid.oregonstate.edu>
2015-08-23 16:32:22 -0700

    Started Swift Delegates

---
+++ Swift Delegates.playground/Contents.swift
@@ -0,0 +1,12 @@
+// Swift Delegates
+
+/*
+ * Design Pattern - A general, reuseable solution to a commonly occuring problem
+ * within a given context, regardless of the particular domain.
+ *
+ * Delegate (Decorator) Pattern - A variation of a design pattern, a structural
+ * pattern that is focused on how we compose objects to form larger
+ * structures.  Specifically this pattern is concerned with how we can add
+ * additional responsibility to an object dynamically, that is at runtime rather
+ * than statically at compile time.
+ */
